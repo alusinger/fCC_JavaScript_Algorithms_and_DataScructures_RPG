56192fc10f243aa292072cebfd3526ed338d6f32
Anthony Lusinger <45246401+alusinger@users.noreply.github.com>
2023-12-27 06:27:08 +0000

    final commit - added easter egg

--- script.js
+++ script.js
@@ -65,9 +65,10 @@ const locations = [
         text: "You are fighting a monster."
     },
     {
+        // Player can access secret easter egg function by selecting the 3rd "Go to town square" button.
         name: "kill monster",
         "button text": ["Go to town square", "Go to town square", "Go to town square"],
-        "button functions": [goTown, goTown, goTown],
+        "button functions": [goTown, goTown, easterEgg],
         text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
     },
     {
